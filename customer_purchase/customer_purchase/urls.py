0d2ca02412845b9a99c567bd905b2f7220963617
Seetharam46 <seetharam.p2004@gmail.com>
2025-02-09 19:18:54 +0530

    Delete customer_purchase directory

--- customer_purchase/customer_purchase/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('purchases.urls')),  # Include the purchases app URLs
-]
